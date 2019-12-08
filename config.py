ef62b19245ab1f046bbc1bd08a6a33fab83e4495
Phoenix <ubuntu@phoenix-ubuntu>
2019-12-08 14:13:41 +0000

    Update config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 Dataset_Path = dict(
     CULane = "/home/lion/Dataset/CULane/data/CULane",
     Tusimple = "/home/lane_detection/datasets/tusimple/unpacked",
-    Phoenix = "/home/lane_detection/datasets/phoenix"
+    Phoenix = "/home/lane_detection/datasets/phoenix/wooden_lounge"
 )
